764134507dc4f0d5bbb12711fd1272f1d0cfff69
Eli Gundry <eligundry@gmail.com>
2022-11-08 03:34:23 -0500

    Add concept of server-timing grouping
    I was reading the RFC and it makes a lot of sense to support groups.
    This did require a fair amount of refactoring, but thank god for tests!

--- src/ServerTiming.test.ts
+++ src/ServerTiming.test.ts
@@ -1,96 +1,146 @@
-import test, { describe, it } from 'node:test'
+import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
 import { ServerTiming } from './ServerTiming'
 
 const asyncFn = async (): Promise<string> =>
   new Promise((resolve) => setTimeout(() => resolve('hello world'), 3))
 
-console.log('hello')
-// describe('ServerTiming', () => {
-test('should manually be able to track how long a function takes', async () => {
-  const serverTiming = new ServerTiming()
-  serverTiming.start('foo')
-  await asyncFn()
-  serverTiming.end('foo')
+describe('ServerTiming', () => {
+  it('should manually be able to track how long a function takes', async () => {
+    const serverTiming = new ServerTiming()
+    serverTiming.start('foo')
+    await asyncFn()
+    serverTiming.end('foo')
 
-  assert.match(serverTiming.header(), /foo;dur=\d/)
-})
-
-test('should be able to chain start and end tracking calls', async () => {
-  const serverTiming = new ServerTiming()
-  serverTiming.start('foo')
-  await asyncFn()
-  serverTiming.end('foo').start('bar')
-  await asyncFn()
-  serverTiming.end('bar')
-
-  assert.match(serverTiming.header(), /foo;dur=\d, bar;dur=\d/)
-})
-
-test('should allow providing of a human readable description of the timer', async () => {
-  const serverTiming = new ServerTiming()
-  serverTiming.start({
-    label: 'foo',
-    desc: 'Foo Service',
+    assert.match(serverTiming.headers()[0], /foo;dur=\d/)
   })
-  await asyncFn()
-  serverTiming.end('foo')
 
-  assert.match(serverTiming.header(), /foo;desc="Foo Service";dur=\d/)
-})
+  it('should be able to chain start and end tracking calls', async () => {
+    const serverTiming = new ServerTiming()
+    serverTiming.start('foo')
+    await asyncFn()
+    serverTiming.end('foo').start('bar')
+    await asyncFn()
+    serverTiming.end('bar')
+    const headers = serverTiming.headers()
 
-test('should be able to wrap an async function and track time', async () => {
-  const serverTiming = new ServerTiming()
-  const returnValue = await serverTiming.track('foo', asyncFn)
+    assert.equal(headers.length, 2)
+    assert.match(serverTiming.headers()[0], /foo;dur=\d/)
+    assert.match(serverTiming.headers()[1], /bar;dur=\d/)
+  })
 
-  assert.equal(returnValue, 'hello world')
-  assert.match(serverTiming.header(), /foo;dur=\d/)
-})
-
-test('should be able to provide description to tracked function', async () => {
-  const serverTiming = new ServerTiming()
-  await serverTiming.track(
-    {
+  it('should allow providing of a human readable description of the timer', async () => {
+    const serverTiming = new ServerTiming()
+    serverTiming.start({
       label: 'foo',
       desc: 'Foo Service',
-    },
-    asyncFn
-  )
+    })
+    await asyncFn()
+    serverTiming.end('foo')
 
-  assert.match(serverTiming.header(), /foo;desc="Foo Service";dur=\d/)
+    assert.match(serverTiming.headers()[0], /foo;desc="Foo Service";dur=\d/)
+  })
+
+  it('should be able to wrap an async function and track time', async () => {
+    const serverTiming = new ServerTiming()
+    const returnValue = await serverTiming.track('foo', asyncFn)
+
+    assert.equal(returnValue, 'hello world')
+    assert.match(serverTiming.headers()[0], /foo;dur=\d/)
+  })
+
+  it('should be able to provide description to tracked function', async () => {
+    const serverTiming = new ServerTiming()
+    await serverTiming.track(
+      {
+        label: 'foo',
+        desc: 'Foo Service',
+      },
+      asyncFn
+    )
+
+    assert.match(serverTiming.headers()[0], /foo;desc="Foo Service";dur=\d/)
+  })
+
+  it('should be able to track multiple function calls', async () => {
+    const serverTiming = new ServerTiming()
+    await serverTiming.track('foo', asyncFn)
+    await serverTiming.track('bar', asyncFn)
+    await serverTiming.track({ label: 'baz', desc: 'Baz Service' }, asyncFn)
+    const headers = serverTiming.headers()
+
+    assert.equal(headers.length, 3)
+    assert.match(serverTiming.headers()[0], /foo;dur=\d/)
+    assert.match(serverTiming.headers()[1], /bar;dur=\d/)
+    assert.match(serverTiming.headers()[2], /baz;desc="Baz Service";dur=\d/)
+  })
+
+  it('should bubble the error up while ending the timer if the callback throws', async () => {
+    const serverTiming = new ServerTiming()
+    const error = new Error('uh-oh all errors')
+
+    try {
+      await serverTiming.track('error', async () => {
+        await asyncFn()
+        throw error
+      })
+    } catch (e) {
+      assert.equal(e, error)
+    }
+
+    assert.match(serverTiming.headers()[0], /error;dur=\d/)
+  })
+
+  it('should throw an error if a timer is ended without being started', async () => {
+    const serverTiming = new ServerTiming()
+    assert.throws(() => serverTiming.end('foo'), {
+      message: `timing 'foo' was never started`,
+    })
+  })
+
+  it('should output the header value when cast to a string', async () => {
+    const serverTiming = new ServerTiming()
+    await serverTiming.track('foo', asyncFn)
+
+    assert.match(String(serverTiming), /foo;dur=\d/)
+  })
+
+  it('should not end timers when outputing header string', async () => {
+    const serverTiming = new ServerTiming()
+    serverTiming.start('foo')
+    await asyncFn()
+    const header1 = serverTiming.headers()[0]
+    await asyncFn()
+    const header2 = serverTiming.headers()[0]
+
+    assert.match(header1, /foo;dur=\d/)
+    assert.match(header2, /foo;dur=\d/)
+    assert.notEqual(header1, header2)
+  })
+
+  it('should be able to do example 1 in the RFC', async () => {
+    // https://www.w3.org/TR/server-timing/#example-1
+    const serverTiming = new ServerTiming()
+
+    serverTiming
+      .group()
+      .note('miss')
+      .note({ label: 'db', dur: 53 })
+      .note({ label: 'app', dur: 47.2 })
+    serverTiming.group().note('customView').note({ label: 'dc', desc: 'atl' })
+    serverTiming.group().track({ label: 'cache', desc: 'Cache Read' }, asyncFn)
+    const headers = serverTiming.headers()
+
+    assert.equal(headers.length, 3)
+    assert.equal(headers[0], 'miss, db;dur=53, app;dur=47.2')
+    assert.equal(headers[1], 'customView, dc;desc="atl"')
+    assert.match(headers[2], /cache;desc="Cache Read";dur=\d/)
+  })
+
+  it('should throw if you attempt to group more than one layer down', () => {
+    const serverTiming = new ServerTiming()
+    assert.throws(() => serverTiming.group().group().note('miss'), {
+      message: 'groups cannot be more than one level deep',
+    })
+  })
 })
-
-test('should be able to track multiple function calls', async () => {
-  const serverTiming = new ServerTiming()
-  await serverTiming.track('foo', asyncFn)
-  await serverTiming.track('bar', asyncFn)
-  await serverTiming.track('baz', asyncFn)
-
-  assert.match(serverTiming.header(), /foo;dur=\d, bar;dur=\d, baz;dur=\d/)
-})
-
-// it('should throw an error if a timer is ended without being started', async () => {
-//   const serverTiming = new ServerTiming()
-//   assert.throws(serverTiming.end('foo'), `timing 'foo' was never started`)
-// })
-
-test('should output the header value when cast to a string', async () => {
-  const serverTiming = new ServerTiming()
-  await serverTiming.track('foo', asyncFn)
-
-  assert.match(String(serverTiming), /foo;dur=\d/)
-})
-
-test('should not end timers when outputing header string', async () => {
-  const serverTiming = new ServerTiming()
-  serverTiming.start('foo')
-  await asyncFn()
-  const header1 = serverTiming.header()
-  await asyncFn()
-  const header2 = serverTiming.header()
-
-  assert.match(header1, /foo;dur=\d/)
-  assert.match(header2, /foo;dur=\d/)
-  assert.notEqual(header1, header2)
-})
-// })
--- src/ServerTiming.ts
+++ src/ServerTiming.ts
@@ -1,6 +1,7 @@
 export type DetailedServerTimingLabel = {
   label: string
   desc?: string
+  dur?: number
 }
 
 export type ServerTimingLabel = string | DetailedServerTimingLabel
@@ -31,6 +32,16 @@ export type ServerTimingLabel = string | DetailedServerTimingLabel
  *   // which allows for wrapping of functions that will track latency.
  *   const orders = await serverTiming.track('db:orders', () => db.getOrders())
  *
+ *   // All tracking calls can accept a human readable description of the
+ *   // tracking call.
+ *   const stats = await serverTiming.track(
+ *     {
+ *       label: 'stats',
+ *       desc: 'Aggregated stats about sales',
+ *     },
+ *     () => db.getStats()
+ *   )
+ *
  *   // When you are done tracking operations, attach headers to the response by
  *   // calling serverTiming.header().
  *   return json({ users, orders }, {
@@ -43,16 +54,16 @@ export type ServerTimingLabel = string | DetailedServerTimingLabel
  */
 export class ServerTiming {
   public headerKey = 'Server-Timing' as const
-  private timings: Map<
-    string,
-    {
-      start: Date
-      end?: Date
-    } & Omit<DetailedServerTimingLabel, 'label'>
-  >
+  private timings: (
+    | (DetailedServerTimingLabel & {
+        start?: Date
+        end?: Date
+      })
+    | ServerTiming
+  )[]
 
   constructor() {
-    this.timings = new Map()
+    this.timings = []
   }
 
   private transformLabel(obj: ServerTimingLabel): DetailedServerTimingLabel {
@@ -63,29 +74,64 @@ export class ServerTiming {
     return obj
   }
 
-  start(labelObj: ServerTimingLabel) {
-    const { label, ...rest } = this.transformLabel(labelObj)
+  note(labelObj: ServerTimingLabel) {
+    this.timings.push(this.transformLabel(labelObj))
+    return this
+  }
 
-    this.timings.set(label, {
+  group() {
+    const group = new ServerTiming()
+    this.timings.push(group)
+    group.group = () => {
+      throw new Error('groups cannot be more than one level deep')
+    }
+
+    return group
+  }
+
+  /**
+   * Manually start tracking the latency of operations. This function is useful
+   * if you have multiple related operations that have assignments that would be
+   * annoying to track using [[track]].
+   *
+   * @example
+   *
+   * ```typescript
+   * const serverTiming = new ServerTiming()
+   * serverTiming.start('db')
+   * const user = db.getUser(1)
+   * const orders = db.getUserOrders(1)
+   * serverTiming.end('db')
+   * ```
+   */
+  start(labelObj: ServerTimingLabel) {
+    const timing = this.transformLabel(labelObj)
+
+    this.timings.push({
+      ...timing,
       start: new Date(),
-      ...rest,
     })
 
     return this
   }
 
+  /**
+   * Manually end a tracking call. This function is to be used in conjunction
+   * with [[ServerTiming.start]].
+   *
+   * @throws An error if the label was not started with [[ServerTiming.start]].
+   */
   end(labelObj: ServerTimingLabel) {
     const { label } = this.transformLabel(labelObj)
-    const timing = this.timings.get(label)
+    const timingIdx = this.timings.findIndex(
+      (t) => !(t instanceof ServerTiming) && t.label === label
+    )
 
-    if (!timing) {
+    if (timingIdx === -1) {
       throw new Error(`timing '${label}' was never started`)
     }
 
-    this.timings.set(label, {
-      ...timing,
-      end: new Date(),
-    })
+    this.timings[timingIdx].end = new Date()
 
     return this
   }
@@ -93,6 +139,16 @@ export class ServerTiming {
   /**
    * Track how long it takes for a function to run and save it to the
    * ServerTiming instance.
+   *
+   * If the function throws an error, the timer will be stopped and the error
+   * will bubble up.
+   *
+   * @example
+   *
+   * ```typescript
+   * const serverTiming = new ServerTiming()
+   * const rows = serverTiming.track('db', () => db.query())
+   * ```
    */
   async track<T extends () => Promise<any> | any>(
     labelObj: ServerTimingLabel,
@@ -108,29 +164,52 @@ export class ServerTiming {
     }
   }
 
-  header(): string {
-    return Array.from(this.timings.entries())
-      .map(([label, timing]) => {
+  /**
+   * Output the value of the `Server-Timing` headers for the functions tracked.
+   * Note that the `ServerTiming` instance provides a memeber for the key of the
+   * header.
+   *
+   * This function is side effect free. If you output the header without ending
+   * a manual tracking call, the end of that timer will be a temporary value of
+   * the current time. This means that subsequent calls to this will advance any
+   * unclosed timers to that date in the outputted string.
+   *
+   * @example
+   *
+   * ```typescript
+   * const headers = new Headers()
+   * headers.set(serverTiming.headerKey, serverTiming.headers())
+   * ```
+   */
+  headers(): string[] {
+    return this.timings.map((timing) => {
+      if (timing instanceof ServerTiming) {
+        return timing.headers().join(', ')
+      }
+
+      const parts = [timing.label]
+
+      if (timing.desc) {
+        parts.push(`desc="${timing.desc}"`)
+      }
+
+      if (timing.dur) {
+        parts.push(`dur=${timing.dur}`)
+      } else if (timing.start) {
         let end = timing.end
 
         if (!end) {
           end = new Date()
         }
 
-        let value = `${label};`
+        parts.push(`dur=${end.getTime() - timing.start.getTime()}`)
+      }
 
-        if (timing.desc) {
-          value += `desc="${timing.desc}";`
-        }
-
-        value += `dur=${end.getTime() - timing.start.getTime()}`
-
-        return value
-      })
-      .join(', ')
+      return parts.join(';')
+    })
   }
 
   toString() {
-    return this.header()
+    return this.headers().join(', ')
   }
 }
